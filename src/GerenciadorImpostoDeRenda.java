246a5aa006308dcf95fb0bb05be2dbbc41c8d2a5
R3nn4 <82388713+R3nn4@users.noreply.github.com>
2023-11-03 17:49:50 -0300

    Mensagem deixada no GerenciadorImpostoDeRenda.java

--- src/GerenciadorImpostoDeRenda.java
+++ src/GerenciadorImpostoDeRenda.java
@@ -3,6 +3,12 @@ public class GerenciadorImpostoDeRenda {
     private double total_IR = 0;
 
     //METODOS
+    /*Não ficou claro se o total_IR deveria ser a soma dos saldos dos tributaveis em si
+    ou se deveria ser a soma do valor cobrado deles pelo Imposto de Renda, então eu
+    decidi deixá-lo como este último: a soma dos valores cobrados. As alterações em si
+    estão nos arquivos da ContaCorrente e do Seguro de Vida, mas achei pertinente deixar a
+    mensagem aqui por ser pra onde os valores dos tributos vêm.*/
+    
     protected void calculaImpostoRenda(Tributavel tributavel){
         total_IR += tributavel.calculaTributos();
     }
